270d032b0e7cb94e84946d274defbcdc31035b23
Sanjay Kumar <sanjay.kumar@wishfin.com>
2021-07-06 17:20:22 +0530

    files added

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from './Header';
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <button>Add</button>
+      <div>
+        <span>Name</span><br />
+        <span>Phone Number</span>
+      </div>      
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = function(){
+    return(
+        <div className="header">its a Phone Directory</div>
+    )
+}
+
+export default Header;
