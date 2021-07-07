4793aff964afd73e1678923b4c2690ef76c257b4
Sanjay Kumar <sanjay.kumar@wishfin.com>
2021-07-07 12:49:26 +0530

    Rendered Subscribers Dynamically

--- src/App.js
+++ src/App.js
@@ -1,16 +1,40 @@
 import React from 'react';
 import Header from './Header';
+import './App.css';
 
 function App() {
+  let subscribers = [
+    {
+      name: "Sanjay",
+      phone: 9889999999
+    },
+    {
+      name: "Raj",
+      phone: 8889999977
+    }
+]
   return (
     <div>
       <Header/>
       <button>Add</button>
-      <div>
-        <span>Name</span><br />
-        <span>Phone Number</span>
+      <div className="field-row">
+        <span className="field-item strong">Name</span>
+        <span className="field-item strong">Phone</span>
       </div>      
-      
+      {/* <div className="field-row">
+        <span className="field-item">Sanjay</span>
+        <span className="field-item">9888888888</span>
+      </div> */}
+      {
+        subscribers.map((item, i)=>{
+          return(
+            <div className="field-row" key={i}>
+              <span className="field-item">{item.name}</span>
+              <span className="field-item">{item.phone}</span>
+            </div>
+          )
+        })
+      }
     </div>
   );
 }
